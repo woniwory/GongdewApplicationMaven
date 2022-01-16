e4ee7b4738140a06db0736c656356295be5a0f62
woniwory <37476534+woniwory@users.noreply.github.com>
2022-01-17 01:00:31 +0900

    Initial commit

---
+++ src/main/java/com/example/gongdewapplicationmaven/model/User.java
@@ -0,0 +1,73 @@
+package com.example.gongdewapplicationmaven.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String userID;
+    private String userPassword;
+    private String userBirth;
+    private String userDepartment;
+    private String userHabit;
+
+    public User() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserBirth() {
+        return userBirth;
+    }
+
+    public void setUserBirth(String userBirth) {
+        this.userBirth = userBirth;
+    }
+
+    public String getUserDepartment() {
+        return userDepartment;
+    }
+
+    public void setUserDepartment(String userDepartment) {
+        this.userDepartment = userDepartment;
+    }
+
+    public String getUserHabit() {
+        return userHabit;
+    }
+
+    public void setUserHabit(String userHabit) {
+        this.userHabit = userHabit;
+    }
+
+}
